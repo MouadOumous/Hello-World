ebe7eb9908c131713c8faf9d386575262e6110ba
MouadOumous <85079344+MouadOumous@users.noreply.github.com>
2021-05-30 21:20:37 +0100

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+# Hello-World
+public class Helloworld {
+
+//CREATE main method to run our Java code 
+
+public static main(String[]arg){
+
+//CALL println method that exist in System class
+
+System.out.println("Hello World");
+
+}//end of main method
+
+}//end of the main class (Hello World class)
